7af66e3bab5e1b41a27a644e659f45a687b2dc7d
David Eng <davideng@madmax2.stanford.edu>
2014-06-25 13:25:56 -0700

    testing

---
+++ examples/deng/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "stdafx.h"
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	cout << "Hello World" << endl;
+	printf("hello\n");
+	cout << "Bubye" << endl;
+	return 0;
+}
