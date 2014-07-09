cecd66eed17a3feb4261a9217cb5e9d4a34e922a
David Eng <davideng@madmax2.stanford.edu>
2014-07-09 12:24:34 -0700

    command-line functionality interface

--- examples/deng/main.cpp
+++ examples/deng/main.cpp
@@ -5,10 +5,16 @@
 #include <string>
 #include "stdafx.h"
 #include <unistd.h>
+#include <cinttypes> // int64_t, PRId64
+#include <cstring> // strcmp
+#include <ctime> // clock_t, clock()
 using namespace std;
 
 #define DATAFILE "/dfs/ilfs2/0/deng/data/glp00ag.asc"
 #define MATRIXFILE "output/matrix.out"
+#define OUTPUTDIR "output"
+#define MAXSIZE 128
+
 // #define DATAFILE "/dfs/ilfs2/0/deng/data/test.asc"
 #define HEADERLINES 6
 #define NCOLSLINE 0
@@ -19,9 +25,13 @@ using namespace std;
 #define PI 3.141592653
 #define ALPHA 10
 
-// DEBUG
-#define SCALE 10
-// DEBUG
+int popScale = 1;
+bool dumpCoords = false, dumpMatrix = false, dumpStats = false;
+FILE *dumpCoordsFile = NULL, *dumpMatrixFile = NULL, *dumpStatsFile = NULL;
+clock_t startTime = clock();
+
+int generation = 0; // DEBUG dumped stats
+int coordinatesAdded = 0; // DEBUG dumped stats
 
 TRnd generator(1, 0);
 
@@ -29,9 +39,9 @@ inline int positive_modulo(int i, int n) {
   return (i % n + n) % n;
 }
 
-// Vector of TInt pairs, int64 indicates the upper-limit of vector size equals
+// Vector of TInt pairs, int64_t indicates the upper-limit of vector size equals
 // 64-bit representation.
-typedef TVec<TIntPr, int64> TIntPrI64Vec;
+typedef TVec<TIntPr, int64_t> TIntPrI64Vec;
 
 // Returns -1 if TStr does not contain a valid number
 // Otherwise returns the number
@@ -77,10 +87,10 @@ void extractHeaders(TSsParser &parser, int &numCols, int &numRows) {
 
 // Loads raw data from "infile" into matrix, requires 5 +/- 1s to load
 // Saves matrix into "outfile"
-// Returns an int64 of total population of the loaded dataset
-int64 load(TVVec<TIntPr> &matrix, const TStr &infile, const TStr &outfile,
+// Returns an int64_t of total population of the loaded dataset
+int64_t load(TVVec<TIntPr> &matrix, const TStr &infile, const TStr &outfile,
           int &numCols, int &numRows) {
-  int64 census = 0;
+  int64_t census = 0;
   TSsParser parser(infile, ' ');
   extractHeaders(parser, numCols, numRows);
   matrix.Gen(numCols, numRows); // Resize matrix to contain ensuing data
@@ -89,7 +99,7 @@ int64 load(TVVec<TIntPr> &matrix, const TStr &infile, const TStr &outfile,
     for (int curCol = 0; curCol < numCols; curCol++) {
       // ceil(atof(x)) returns the ceiling of a string represented in sci-notation as an int.
       // "init" represents initial count of individuals occupying coordinate (curCol, curRow)
-      int init = ceil(atof(parser.GetFld(curCol)) / SCALE);
+      int init = ceil(atof(parser.GetFld(curCol)) / popScale);
       census += init;
       TIntPr initPair(init, init);
       matrix.PutXY(curCol, curRow, initPair);
@@ -101,14 +111,14 @@ int64 load(TVVec<TIntPr> &matrix, const TStr &infile, const TStr &outfile,
 }
 
 // Loads saved data from "infile" into matrix, requires 1 +/- 1s to load
-// Returns an int64 of total population of the loaded dataset
-int64 load(TVVec<TIntPr> &matrix, const TStr &infile, int &numCols,
+// Returns an int64_t of total population of the loaded dataset
+int64_t load(TVVec<TIntPr> &matrix, const TStr &infile, int &numCols,
           int &numRows) {
   { TFIn in(infile);
     matrix.Load(in); }
   numCols = matrix.GetXDim();
   numRows = matrix.GetYDim();
-  int64 census = 0;
+  int64_t census = 0;
   int numInitial, numRemaining;
   for (int curRow = 0; curRow < numRows; curRow++) {
     for (int curCol = 0; curCol < numCols; curCol++) {
@@ -170,11 +180,6 @@ void selectEnd(TVVec<TIntPr> &matrix, int startCol, int startRow,
   endRow = positive_modulo(static_cast<int>(startRow + (distance * sin(angle))), numRows);
 }
 
-int generation = 0;
-FILE *coordsDump = fopen("output/glp00ag.out", "w");
-FILE *sigintDump = fopen("output/10", "w");
-int coordinatesAdded = 0; // DEBUG
-
 // For each individual in cur, use power-law distribution to select individuals
 // to append to next.
 // Currently hardcode the number of friends of each cur individual. Possible
@@ -204,7 +209,9 @@ void select(TVVec<TIntPr> &matrix, TIntPrI64Vec &startVec,
       } while (endNumInitial == 0);
       if (endNumRemaining > 0) {
         // DEBUG
-        fprintf(coordsDump, "(%d,%d) (%d,%d)\n", endCol, endRow, startCol, startRow);
+        if (dumpCoords) {
+          fprintf(dumpCoordsFile, "(%d,%d) (%d,%d)\n", endCol, endRow, startCol, startRow);
+        }
         coordinatesAdded++;
         // DEBUG
 
@@ -216,7 +223,7 @@ void select(TVVec<TIntPr> &matrix, TIntPrI64Vec &startVec,
   }
 }
 
-void simulate(TVVec<TIntPr> &matrix, int numCols, int numRows, int64 census) {
+void simulate(TVVec<TIntPr> &matrix, int numCols, int numRows, int64_t census) {
   // long vector with 32-bit integer values and 64-bit indices:
   TIntPrI64Vec curVec(0);
   TIntPrI64Vec nextVec(0);
@@ -229,6 +236,7 @@ void simulate(TVVec<TIntPr> &matrix, int numCols, int numRows, int64 census) {
     generation++;
     curVec = nextVec;
     nextVec.Clr();
+    if (dumpStats) fprintf(dumpStatsFile, "Gen %d : Pop %" PRId64 "\n", generation, curVec.Len());
     select(matrix, curVec, nextVec, numCols, numRows);
   }
 }
@@ -254,41 +262,86 @@ void printMatrix(TVVec<TIntPr> &matrix, FILE *outfile) {
   }
 }
 
-// DEBUG
-TVVec<TIntPr> matrix(0, 0);
-// DEBUG
 
-// DEBUG
+TVVec<TIntPr> matrix(0, 0); // DEBUG, ultimately declare as local in main
+
 void my_handler(int param) {
   printf("\nBOOM GOES THE SIGINT!\n");
-  printf("Coordinates Added: %d\n", coordinatesAdded);
-  printf("Current Generation: %d\n", generation);
-  printf("Writing relevant statistics and current state of the matrix to\n");
-  printf("file \"output/sigintDump.out\"......\n");
-  fprintf(sigintDump, "Coordinates Added: %d\n", coordinatesAdded);
-  fprintf(sigintDump, "Current Generation: %d\n", generation);
-  printMatrix(matrix, sigintDump);
+  if (dumpStats) {
+    printf("Dumping your stats......\n");
+    clock_t endTime = clock();
+    fprintf(dumpStatsFile, "Coordinates Added: %d\n", coordinatesAdded);
+    fprintf(dumpStatsFile, "Current Generation: %d\n", generation);
+    fprintf(dumpStatsFile, "Time taken: %.2fs\n", ((float) endTime - (float) startTime) / 1000000 );
+  }
+  if (dumpMatrix) {
+    printf("Dumping your matrix......\n");
+    printMatrix(matrix, dumpMatrixFile);
+  }
   exit(1);
 }
-// DEBUG
 
 int main(int argc, char* argv[]) {
-  // DEBUG
+  for (int i = 0; i < argc; i++) {
+    if (strncmp(argv[i], "-dump=coords", strlen("-dump=coords")) == 0) {
+      dumpCoords = true;
+    }
+    if (strncmp(argv[i], "-dump=matrix", strlen("-dump=matrix")) == 0) {
+      dumpMatrix = true;
+    }
+    if (strncmp(argv[i], "-dump=stats", strlen("-dump=stats")) == 0) {
+      dumpStats = true;
+    }
+    if (strncmp(argv[i], "-scale=", strlen("-scale=")) == 0) {
+      int scale = extractNum((char*) argv[i] + strlen("-scale="));
+      if (scale >= 1 && scale <= 10000) popScale = scale;
+    }
+    if (strncmp(argv[i], "-usage", strlen("-usage")) == 0) {
+      printf("$ ./main [FLAGS ...]                                        \n");
+      printf("-dump=coords : output the series of coordinates of buckets visited during BFS   \n");
+      printf("-dump=matrix : output the state of the matrix i.e. the two-dimensional array of \n");
+      printf("               buckets, each of which stores two values: 1) number of intial    \n");
+      printf("               people occupying the bucket and 2) number of remaining people at \n");
+      printf("               the time of the dump. The dump will occur in response to SIGINT  \n");
+      printf("               or at completion.                                                \n");
+      printf("-dump=stats  : output the relevant statistics                                   \n");
+      printf("-scale=%%d    : scales down the initial population in each bucket by %%d          \n");
+      exit(0);
+    }
+  }
+
   void (*prev_handler) (int);
   prev_handler = signal(SIGINT, my_handler);
-  // DEBUG
+
+  if (dumpCoords) {
+    char buffer[MAXSIZE];
+    sprintf(buffer, "%s/coords.sc%d.out", OUTPUTDIR, popScale);
+    dumpCoordsFile = fopen(buffer, "w");
+    printf("Writing coordinates to file \"%s\"\n", buffer);
+  }
+
+  if (dumpMatrix) {
+    char buffer[MAXSIZE];
+    sprintf(buffer, "%s/matrix.sc%d.dump", OUTPUTDIR, popScale);
+    dumpMatrixFile = fopen(buffer, "w");
+    printf("Dumping matrix to file \"%s\"\n", buffer);
+  }
+
+  if (dumpStats) {
+    char buffer[MAXSIZE];
+    sprintf(buffer, "%s/stats.sc%d.dump", OUTPUTDIR, popScale);
+    dumpStatsFile = fopen(buffer, "w");
+    printf("Dumping stats to file \"%s\"\n", buffer);
+  }
+
+  fflush(stdout);
 
   int numCols = -1, numRows = -1;
   // TVVec<TIntPr> matrix(0, 0);
-  int64 census = load(matrix, DATAFILE, MATRIXFILE, numCols, numRows);
-  // int64 census = load(matrix, MATRIXFILE, numCols, numRows);
-  // printMatrix(matrix, NULL);
-  printf("pop: %I64d\nnumCols: %d\nnumRows: %d\n", census, numCols, numRows);
-  // simulate(matrix, numCols, numRows, census);
-
-  // DEBUG
+  int64_t census = load(matrix, DATAFILE, MATRIXFILE, numCols, numRows);
+  // int64_t census = load(matrix, MATRIXFILE, numCols, numRows);
+  printf("pop: %" PRId64 "\nnumCols: %d\nnumRows: %d\n", census, numCols, numRows);
+  simulate(matrix, numCols, numRows, census);
   kill(getpid(), SIGINT);
-  // DEBUG
-
   return 0;
 }
